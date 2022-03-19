53869de7751c145735a66cba1c33ad859bebe4c9
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-03-19 20:14:42 +0900

    Add SMUI

--- src/App.svelte
+++ src/App.svelte
@@ -1,10 +1,12 @@
 <script lang="ts">
+	import Button from "@smui/button";
 	export let name: string;
 </script>
 
 <main>
 	<h1>Hello {name}!</h1>
 	<p>Visit the <a href="https://svelte.dev/tutorial">Svelte tutorial</a> to learn how to build Svelte apps.</p>
+	<Button>123</Button>
 </main>
 
 <style>
