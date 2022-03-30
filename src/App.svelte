7bfd770dea4dfc8502261e0c593e65519dcffcbe
ShinYeongHyeon <den.shin.dev@gmail.com>
2022-03-31 00:32:06 +0900

    Implement 404 Page

--- src/App.svelte
+++ src/App.svelte
@@ -4,4 +4,12 @@
   import { routes } from './router/router';
 </script>
 
-<Router {routes} />
+<main>
+    <Router {routes} />
+</main>
+
+<style>
+    main {
+        padding: 2em 1em;
+    }
+</style>
--- src/pages/NotFoundPages.svelte
+++ src/pages/NotFoundPages.svelte
@@ -1,3 +1,16 @@
+<script>
+    import { Headline, Subhead } from 'attractions';
+    import { navigateTo } from 'svelte-router-spa';
+
+    function backToHome() {
+      navigateTo('/');
+    }
+</script>
+
 <div>
-    404 Page
+    <Headline>Oops..</Headline>
+    <Subhead>
+        I think you access wrong URL... T.T<br />
+        <span on:click={backToHome}>Back to HOME</span>
+    </Subhead>
 </div>
